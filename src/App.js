14e76171378a7b0e1d29404c966b79fb61a79371
William Leung <leungwk25@gmail.com>
2018-08-12 01:13:26 -0700

    Refactor App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React,{Component} from 'react';
-import World from './features/world'
+import World from './features/world';
 import './style.scss';
-
-//import Container from './features/Container';
-// import Main from './features/Main';
 
 class App extends Component{
   render(){
